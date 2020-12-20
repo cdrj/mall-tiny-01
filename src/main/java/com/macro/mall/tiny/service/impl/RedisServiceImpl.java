c9b2d7be8106f0cfb6df15dbaf627b9dc5b25054
drj <836335723@qq.com>
2020-12-20 14:37:31 +0800

    整合了jwt和SpringSecurity实现认证和授权

--- src/main/java/com/macro/mall/tiny/service/impl/RedisServiceImpl.java
+++ src/main/java/com/macro/mall/tiny/service/impl/RedisServiceImpl.java
@@ -2,7 +2,6 @@ package com.macro.mall.tiny.service.impl;
 
 import com.macro.mall.tiny.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
